53597ab106fb2020ed86e3f9332da887da7bb1df
xxxNdr <andrea1987andre@gmail.com>
2025-07-31 12:51:24 +0200

    es7 impostato

---
+++ esercizi_estate/es7/inc/pagine.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$pagina = [
+    'homepage' => [
+        'body' => [
+            'titolo' => 'Calcola distanza tragitto',
+            'template' => 'tpl/form.html',
+            'include' => 'lib/render.php'
+        ]
+    ]
+];
---
+++ esercizi_estate/es7/index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+/* esercizio 07
+Scrivere un programma che consenta agli utenti di inserire un itinerario composto da
+un numero arbitrario di tappe; il programma deve contenere un array associativo nel quale
+siano memorizzate le distanze fra ogni tappa.
+Calcolare la distanza totale coperta dall'itinerario scelto dall'utente. */
